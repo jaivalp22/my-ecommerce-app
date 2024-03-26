f99104bda63ecd7d5421543e8b0955f840ed5b10
JaisumerS <sumer.sandhu@hotmail.com>
2024-03-25 23:48:33 -0600

    b

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -5,9 +5,11 @@ import productsData from '../data/products';
 export default function ProductList({ cartadder }) {
   return (
     <div>
+
       {productsData.map(product => (
         <ProductItem key={product.id} product={product} cartadder={cartadder} />
       ))}
+
     </div>
   );
 }
