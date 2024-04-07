cbd4f396dcc767f51f7e4dcc6bb484d29132bb84
JaisumerS <sumer.sandhu@hotmail.com>
2024-04-07 04:12:29 -0600

    Bleh

--- src/component/authenticator.js
+++ src/component/authenticator.js
@@ -5,12 +5,8 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ people }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false);
-  const isAuthenticated = () => isLoggedIn;
-
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout, isAuthenticated }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn}}>
       {people}
     </AuthContext.Provider>
   );
